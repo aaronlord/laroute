fda6ecaf5c4d9399fe3661317cff5c1973712e9c
Aaron Lord <aaronlord1@gmail.com>
2014-03-14 12:24:04 +0000

    Updated. Documentation to the config file

--- src/config/config.php
+++ src/config/config.php
@@ -3,15 +3,26 @@
 return array(
 
     /**
-     * Path to the template laroute javascript file. This is the file that contains
-     * the functions for
+     * The destination path for the javascript file.
      */
-    'template' => 'vendor/lord/laroute/src/Lord/Laroute/templates/laroute.min.js',
-
     'path'     => 'public/js',
 
+    /**
+     * The destination filename for the javascript file.
+     */
     'filename' => 'laroute',
 
+    /**
+     * The namespace for the helper functions. By default this will bind them to
+     * `window.laroute`.
+     */
     'namespace' => 'laroute',
+
+    /**
+     * The path to the template `laroute.js` file. This is the file that contains
+     * the ported helper Laravel url/route functions and the route data to go
+     * with them.
+     */
+    'template' => 'vendor/lord/laroute/src/Lord/Laroute/templates/laroute.min.js',
 
 );
